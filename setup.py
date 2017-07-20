851895277c33982d042288c15aa9189738756ae8
Anonymission <marrs2k@gmail.com>
2017-07-20 15:48:22 -0400

    added setup

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+try:
+    import camera
+except ImportError:
+    print("Warning: `camera` must be installed in order to use `face_recognition`")
+
+try:
+    import dlib_models
+except ImportError:
+    print("Warning: `dlib_models` must be installed in order to use `face_recognition`")
+
+try:
+    import skimage.io
+except ImportError:
+    print("Warning: `skimage.io` must be installed in order to use `face_recognition`")
+
+
+setup(name='audio_identification',
+      version='1.0',
+      description='Identifies audio samples',
+      authors='Petar Griggs (@Anonymission), Michael Lai (@impostercafe), Amanda Wang (@CandyMandy28), Anthony Cavallaro (@QuantatativeFinancier)',
+      author_email="marrs2k@gmail.com",
+      packages=find_packages(),
+      license="MIT"
+      )
