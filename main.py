c5d0f4ee650f3fbc0b921a6dcaa43fd6e7962c7c
impostercafe <mlai8088@gmail.com>
2017-07-20 16:41:55 -0400

    Now detects multiple faces

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from camera import test_camera, take_picture
-
 from dlib_models import load_dlib_models
 
+# this loads the dlib models into memory. You should only import the models *after* loading them.
+# This does lazy-loading: it doesn't do anything if the models are already loaded.
 load_dlib_models()
 
 from dlib_models import models  # must be called after loading the models
@@ -12,15 +13,13 @@ img_array = take_picture()
 
 face_detect = models["face detect"]
 
+# Number of times to upscale image before detecting faces.
+# When would you want to increase this number?
 upscale = 1
 
 detections = face_detect(img_array, upscale)  # returns sequence of face-detections
 detections = list(detections)
 
-det = detections[0] # first detected face in image
-
-# bounding box dimensions for detection
-l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
 
 # Create figure and axes
 fig,ax = plt.subplots(1)
@@ -28,14 +27,16 @@ fig,ax = plt.subplots(1)
 # Display the image
 ax.imshow(img_array)
 
-print(l)
-print(r)
-print(t)
-print(b)
-# Create a Rectangle patch
-rect = patches.Rectangle((l, t), r-l, b-t ,linewidth=1,edgecolor='r',facecolor='none')
+for rectangle in detections:
+    det = rectangle # first detected face in image
 
-# Add the patch to the Axes
-ax.add_patch(rect)
+    # bounding box dimensions for detection
+    l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
+
+    # Create a Rectangle patch
+    rect = patches.Rectangle((l, t), r-l, b-t ,linewidth=1,edgecolor='r',facecolor='none')
+
+    # Add the patch to the Axes
+    ax.add_patch(rect)
 
 plt.show()
