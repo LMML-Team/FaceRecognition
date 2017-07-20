7f7228c3470fc35e882b8b835d58fb8aba0c1e35
Anthony Cavallaro <ajcavallaro10@gmail.com>
2017-07-20 14:18:05 -0400

    Consolidated

--- Detection.py
+++ Detection.py
@@ -1,13 +1,24 @@
+import skimage.io as io
+import numpy as np
+import dlib_models
+from dlib_models import load_dlib_models
+
+load_dlib_models()
+
+from dlib_models import models
+
+
 def face_detection(filepath):
-    import skimage.io as io
-    import numpy as np
-    import dlib_models
-    from dlib_models import load_dlib_models
-
-    load_dlib_models()
-
-    from dlib_models import models
-
+    """
+    Detects faces in an image, makes borders around them, and gives their descriptors
+    
+    Parameters
+    --------------
+    
+    Returns
+    --------------
+    
+    """
     img_array = io.imread(filepath)
 
     if np.size(img_array, 2) == 4:
@@ -20,8 +31,43 @@ def face_detection(filepath):
     detections = face_detect(img_array, upscale)
     detections = list(detections)
 
-    borders(detections)
-    descriptors(detections)
+    for i in range(len(detections)):
+        borders(detections[i])
+        descriptors(detections[i])
 
-    return detections
 
+def borders(det):
+    """
+    Calculates the borders for image
+    
+    
+    
+    """
+
+    l, r, t, b = det.left(), det.right(), det.top(), det.bottom()
+    borders = [l, r, t, b]
+    return borders
+
+
+def descriptors(det):
+    """
+    Calculates and returns descriptors for all detected faces in image
+
+    Parameters
+    -----------------------
+    detections: List of detected faces
+
+    Returns
+    ----------------------
+    descriptors: List of descriptor numpy arrays
+
+    """
+
+
+
+    face_rec_model = models["face rec"]
+    
+    shape = shape_predictor(img_array, det)
+    descriptors = np.array(face_rec_model.compute_face_descriptor(img_array, shape))
+
+    return descriptors
